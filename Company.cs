09737dfe635b5f275bd93fd0ef9ccb79a8eb94eb
MahboobeSimorgh <85565733+MahboobeSimorgh@users.noreply.github.com>
2021-08-10 16:31:29 +0430

    Add files via upload

---
+++ Company.cs
@@ -0,0 +1,25 @@
+﻿//namespace MyApplication.Models
+namespace Models
+{
+	public class Company : object
+	//public class User : System.Object
+	{
+		public Company() : base()
+		{
+		}
+
+		public int Id { get; set; }
+
+		public string Companyname { get; set; }
+		//public string UserName { get; set; }
+
+		public string Product { get; set; }
+		//public string PassWord { get; set; }
+
+		public int Age { get; set; }
+
+		//public string IP { get; set; }
+
+		//public System.DateTime RegisterDateTime { get; set; }
+	}
+}
